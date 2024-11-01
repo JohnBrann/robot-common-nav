aea30588034161792a03eb0fd78295394d5bcdaa
John Brann <john.brann4315@gmail.com>
2024-11-01 12:28:41 -0400

    fixed command line arguments for world name, changed launch file name from turtlebot_world to turtlebot_gazebo_cartographer

---
+++ launch/turtlebot_gazebo_cartographer.launch.py
@@ -0,0 +1,71 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    pkg_robot_common_sim = get_package_share_directory('robot-common-sim')
+    pkg_turtlebot3_gazebo = get_package_share_directory('turtlebot3_gazebo')
+    pkg_turtlebot3_cartographer = get_package_share_directory('turtlebot3_cartographer')
+
+    # Declare launch argument for the world name
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='4x4m_empty.world',
+        description='Name of gazebo world to load'
+    )
+
+    # world_dir = pkg_robot_common_sim + '/worlds/'
+    # world_path = world_dir + world_arg
+
+    sim_time = DeclareLaunchArgument(
+       'use_sim_time', default_value='true',
+       description='use_sim_time or not'
+    )
+
+    # Declare launch arguments for the spawn position
+    x_position = DeclareLaunchArgument(
+        'x_pose', default_value='-1.8',
+        description='X position of the robot'
+    )
+    y_position = DeclareLaunchArgument(
+        'y_pose', default_value='-1.8',
+        description='Y position of the robot'
+    )
+
+    # Launch file to bringup Gazebo and spawn in Turtlebot3
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_turtlebot3_gazebo, 'launch', 'turtlebot3_world.launch.py')
+        ),
+        launch_arguments={
+            'world': LaunchConfiguration('world'),
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'x_pose': LaunchConfiguration('x_pose'),
+            'y_pose': LaunchConfiguration('y_pose')
+        }.items()
+    )
+
+    # Include your Gazebo launch file
+    rviz_cartographer_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_turtlebot3_cartographer, 'launch', 'cartographer.launch.py') 
+        )
+    )
+
+    # Define the launch description
+    ld = LaunchDescription()
+
+    # Add launch options and actions
+    ld.add_action(world_arg)
+    ld.add_action(sim_time)
+    ld.add_action(x_position)
+    ld.add_action(y_position)
+    ld.add_action(gazebo_launch)
+    ld.add_action(rviz_cartographer_launch)
+
+    return ld
