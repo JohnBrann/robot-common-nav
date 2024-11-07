dfd60ab259dfd9f287f3a8752fd70fe5db66f5fc
John Brann <john.brann4315@gmail.com>
2024-11-07 16:46:50 -0500

    modified file extensions in launch folder so it can also recognize xml files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),
+        ('share/' + package_name + '/launch', glob.glob('launch/*.launch')),
         ('share/' + package_name + '/maps', glob.glob('maps/*')),
 
     ],
